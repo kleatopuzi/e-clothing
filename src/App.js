5b2712f5086180f6a11a6848889fc9d30ced88bf
kleatopuzi <ktopuzii@gmail.com>
2022-05-19 05:42:01 +0200

    add header & link

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import "./App.css";
 // import HatsPage from "./hats";
 import HomePage from "./pages/homepage/homepage.cmp";
 import ShopPage from "./pages/shop/shop.cmp";
-
+import Header from "./components/header/header.cmp";
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/shop" element={<ShopPage />}></Route>
