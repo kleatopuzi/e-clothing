87148b8d6a0096494ea61225947d5dcfaab63bda
kleatopuzi <ktopuzii@gmail.com>
2022-05-19 08:03:12 +0200

    add login

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.cmp";
 import ShopPage from "./pages/shop/shop.cmp";
 import Header from "./components/header/header.cmp";
+import SignInAndSignUpPage from "./pages/signin-signup/signIn-signUp.cmp";
+
 function App() {
   return (
     <div>
@@ -14,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/shop" element={<ShopPage />}></Route>
+        <Route path="/signin" element={<SignInAndSignUpPage />}></Route>
       </Routes>
       {/* <HomePage /> */}
     </div>
---
+++ src/pages/signin-signup/signIn-signUp.cmp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./signIn-signUp.style.scss";
+
+import SignIn from "../../components/sign-in/sign-in.cmp";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
