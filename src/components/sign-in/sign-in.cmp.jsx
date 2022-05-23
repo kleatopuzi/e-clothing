082393eb8254c2d9b955747b8350217a15baf207
kleatopuzi <ktopuzii@gmail.com>
2022-05-23 11:19:41 +0200

    sign-in handleSubmit

--- src/components/sign-in/sign-in.cmp.jsx
+++ src/components/sign-in/sign-in.cmp.jsx
@@ -3,7 +3,7 @@ import "./sign-in.styles.scss";
 
 import FormInput from "../form-input/form-input.cmp";
 import CustomButton from "../custom-button/custom-button.cmp";
-import { signInWithGoogle } from "../../firebase/firebase-utils.js";
+import { auth, signInWithGoogle } from "../../firebase/firebase-utils.js";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,10 +14,17 @@ class SignIn extends React.Component {
       password: "",
     };
   }
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
-    // console.log(event);
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
   handleChange = (event) => {
     const { value, name } = event.target;
