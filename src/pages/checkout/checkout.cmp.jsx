0b35187bde4a3201d93941434bf5c0daddff6da2
kleatopuzi <ktopuzii@gmail.com>
2022-05-24 17:27:56 +0200

    added checkout items

--- src/pages/checkout/checkout.cmp.jsx
+++ src/pages/checkout/checkout.cmp.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import CartItem from "../../components/cart-item/cart-item.cmp";
+import CheckoutItem from "../../components/checkout-item/checkout-item.cmp";
+// import CartItem from "../../components/cart-item/cart-item.cmp";
 import {
   selectCartItems,
   selectCartTotal,
@@ -28,7 +29,9 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => cartItem.name)}
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className="total">
       <span>TOTAL: ${total} </span>
     </div>
