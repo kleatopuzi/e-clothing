0dcf0e4702e5dba91d04fbf122c22090877ded90
kleatopuzi <ktopuzii@gmail.com>
2022-05-24 13:16:08 +0200

    bind cart dropdown

--- src/components/cart-dropdown/cart-dropdown.cmp.jsx
+++ src/components/cart-dropdown/cart-dropdown.cmp.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import CustomButton from "../custom-button/custom-button.cmp";
+import CartItem from "../cart-item/cart-item.cmp";
 
 import "./cart.dropdown.style.scss";
 
-const CartDropdown = () => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
+const CartDropdown = ({ cartItems }) => {
+  console.log("cart-item2", cartItems);
+
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {/* {console.log("cart-item2aaaaaa", cartItems)} */}
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton> GO TO CHECKOUT</CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
