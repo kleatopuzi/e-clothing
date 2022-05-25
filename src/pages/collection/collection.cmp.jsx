f3f88b857967e9ae12d72a6a93f75b6b9e060bc8
kleatopuzi <ktopuzii@gmail.com>
2022-05-25 18:23:36 +0200

    collection routing selector

--- src/pages/collection/collection.cmp.jsx
+++ src/pages/collection/collection.cmp.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selector";
 import CollectionItem from "../../components/collection-item/collection-item.cmp";
 import "./collection.style.scss";
 
-const CollectionPage = () => (
-  <div className="category">
-    <h2>CATEGORY PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  console.log("LOG", collection);
+  return (
+    <div className="category">
+      <h2>CATEGORY PAGE</h2>
+    </div>
+  );
+};
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps)(state),
+});
 
-export default CollectionPage;
+export default connect(mapStateToProps)(CollectionPage);
