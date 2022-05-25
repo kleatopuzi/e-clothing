fbb5562afc10551d372da260adae514eeb3a4349
kleatopuzi <ktopuzii@gmail.com>
2022-05-25 14:15:13 +0200

    sync redux-persist store sessonstorage like

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>
