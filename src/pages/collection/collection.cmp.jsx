3712f85696c513f12e059c52040d7ef77620f132
kleatopuzi <ktopuzii@gmail.com>
2022-05-26 10:52:19 +0200

    collection routing selector fixed w useParams

--- src/pages/collection/collection.cmp.jsx
+++ src/pages/collection/collection.cmp.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selector";
+import { useLocation, useParams } from "react-router-dom";
 import CollectionItem from "../../components/collection-item/collection-item.cmp";
 import "./collection.style.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log("LOG", collection);
+  const params = useParams();
+  collection = params;
+  // let location = useLocation();
+  // params = collection;
+  // console.log("LOG", collection);
+  console.log("LOG", params, collection);
   return (
     <div className="category">
       <h2>CATEGORY PAGE</h2>
@@ -13,7 +19,7 @@ const CollectionPage = ({ collection }) => {
   );
 };
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps)(state),
+  collection: selectCollection(ownProps.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
