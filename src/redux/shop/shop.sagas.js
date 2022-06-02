f54be5b55720dd9cda1f47d469f64188da052ec8
kleatopuzi <ktopuzii@gmail.com>
2022-06-02 17:29:41 +0200

    add shopsaga to rootsaga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from "redux-saga/effects";
+import { all, call, put, takeLatest } from "redux-saga/effects";
 import ShopActionTypes from "./shop.types";
 import {
   fetchCollectionsSuccess,
@@ -28,4 +28,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
