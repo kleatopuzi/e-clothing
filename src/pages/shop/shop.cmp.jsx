b846bebfc670536953d2594854fc19d4f96654d7
kleatopuzi <ktopuzii@gmail.com>
2022-06-03 14:00:22 +0200

    useEffect on shopPage cmp

--- src/pages/shop/shop.cmp.jsx
+++ src/pages/shop/shop.cmp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { connect } from "react-redux";
 import "./shop.style.scss";
@@ -7,26 +7,24 @@ import { fetchCollectionsStart } from "../../redux/shop/shop.action";
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionsPageContainer from "../collection/collection.container";
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+// class ShopPage extends React.Component {
+const ShopPage = () => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
-  render() {
-    // const { isCollectionLoaded } = this.props;
-    return (
-      <div className="shop-page">
-        <Routes>
-          <Route path="/" element={<CollectionsOverviewContainer />}></Route>
-          <Route
-            path=":collectionId"
-            element={<CollectionsPageContainer />}
-          ></Route>
-        </Routes>
-      </div>
-    );
-  }
-}
+  }, [fetchCollectionsStart]);
+
+  return (
+    <div className="shop-page">
+      <Routes>
+        <Route path="/" element={<CollectionsOverviewContainer />}></Route>
+        <Route
+          path=":collectionId"
+          element={<CollectionsPageContainer />}
+        ></Route>
+      </Routes>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
