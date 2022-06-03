60600bc13eb2d48aa9c8d5a9dd73536c0a05ec59
kleatopuzi <ktopuzii@gmail.com>
2022-06-03 17:20:47 +0200

    build server in progres

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import path from "path";
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
-// var cors = require('cors')
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cros());
+app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
@@ -25,4 +25,13 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, (error) => {
   if (error) throw error;
   console.log("Server running on port" + port);
+});
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {});
 });
